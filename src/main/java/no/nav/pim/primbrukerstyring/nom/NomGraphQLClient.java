ae9b4d5155219896cd4d245e91df1705a6c59076
Balder Riiser Haugerud <Balder.Riiser.Haugerud@nav.no>
2024-03-12 15:36:34 +0100

    1253: Orgenhet med stor E

--- src/main/java/no/nav/pim/primbrukerstyring/nom/NomGraphQLClient.java
+++ src/main/java/no/nav/pim/primbrukerstyring/nom/NomGraphQLClient.java
@@ -38,35 +38,35 @@ public class NomGraphQLClient {
         log.info("Henter leders resurser for navident {}", navident);
         String document =
                 """
-                        query LedersRessurser {
-                            ressurs(where: {navident: "%s"}) {
-                                navident
-                                visningsnavn
-                                lederFor {
+                query LedersRessurser {
+                    ressurs(where: {navident: "%s"}) {
+                        navident
+                        visningsnavn
+                        lederFor {
+                            orgEnhet {
+                                koblinger {
+                                    ressurs {
+                                        navident
+                                        visningsnavn
+                                    }
+                                }
+                                navn
+                                organiseringer {
                                     orgEnhet {
-                                        koblinger {
+                                        navn
+                                        leder {
                                             ressurs {
                                                 navident
                                                 visningsnavn
-                                            }
-                                        }
-                                        navn
-                                        organiseringer {
-                                            orgEnhet {
-                                                navn
-                                                leder {
-                                                    ressurs {
-                                                        navident
-                                                        visningsnavn
-                                                    }
-                                                }
                                             }
                                         }
                                     }
                                 }
                             }
                         }
-                        """.formatted(navident);
+                    }
+                }
+                """.formatted(navident);
 
         try {
             HttpGraphQlClient graphQlClient = HttpGraphQlClient.create(webClient).mutate().header("Authorization", oidcUtil.getAuthHeader(authorization, scope)).build();
