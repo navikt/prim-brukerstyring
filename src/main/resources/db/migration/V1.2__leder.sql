2c4416023c509a73b8d510b94d4155b0ab65d6a9
Balder Riiser Haugerud <Balder.Riiser.Haugerud@nav.no>
2024-03-15 10:12:20 +0100

    1253: Skrivefeil i sql script

--- src/main/resources/db/migration/V1.2__leder.sql
+++ src/main/resources/db/migration/V1.2__leder.sql
@@ -1,9 +1,9 @@
 CREATE TABLE leder (
     ident TEXT NOT NULL,
-    navn TEXT
+    navn TEXT,
     CONSTRAINT PK_bruker PRIMARY KEY (ident)
 );
 
 ALTER TABLE bruker ADD COLUMN representert_leder_ident TEXT;
-ALTER TABLE bruker ADD CONSTRAINT FK_representert_leder_ident FOREIGN KEY (representert_leder_ident) REFERENCES leder (ident)
+ALTER TABLE bruker ADD CONSTRAINT FK_representert_leder_ident FOREIGN KEY (representert_leder_ident) REFERENCES leder (ident);
 ALTER TABLE bruker ADD COLUMN sist_aksessert TIMESTAMP;
