b7ffc684d6d070bacbb69a0048a9f836babf93d0
Balder Riiser Haugerud <Balder.Riiser.Haugerud@nav.no>
2024-04-09 14:25:12 +0200

    1253: leder_id

--- src/main/resources/db/migration/V1.2__leder.sql
+++ src/main/resources/db/migration/V1.2__leder.sql
@@ -8,5 +8,5 @@ CREATE TABLE leder (
 );
 
 ALTER TABLE bruker ADD COLUMN representert_leder_id TEXT;
-ALTER TABLE bruker ADD CONSTRAINT FK_representert_leder_id FOREIGN KEY (representert_leder_id) REFERENCES leder (id);
+ALTER TABLE bruker ADD CONSTRAINT FK_representert_leder_id FOREIGN KEY (representert_leder_id) REFERENCES leder (leder_id);
 ALTER TABLE bruker ADD COLUMN sist_aksessert TIMESTAMP;
