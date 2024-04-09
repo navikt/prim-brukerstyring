573f2fb6f4d20faf4d0aaad6f4a003e9ebaecb7d
Balder Riiser Haugerud <Balder.Riiser.Haugerud@nav.no>
2024-04-09 14:14:53 +0200

    1253: trailing parentasees

--- src/main/resources/db/migration/V1.2__leder.sql
+++ src/main/resources/db/migration/V1.2__leder.sql
@@ -8,5 +8,5 @@ CREATE TABLE leder (
 );
 
 ALTER TABLE bruker ADD COLUMN representert_leder_ident TEXT;
-ALTER TABLE bruker ADD CONSTRAINT FK_representert_leder_ident FOREIGN KEY (representert_leder_ident REFERENCES leder (ident);
+ALTER TABLE bruker ADD CONSTRAINT FK_representert_leder_ident FOREIGN KEY (representert_leder_ident) REFERENCES leder (ident);
 ALTER TABLE bruker ADD COLUMN sist_aksessert TIMESTAMP;
