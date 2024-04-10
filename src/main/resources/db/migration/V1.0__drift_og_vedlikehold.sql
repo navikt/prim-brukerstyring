8e63cd7ecd08e603288afac688cd7c5ea437db23
Balder Riiser Haugerud <Balder.Riiser.Haugerud@nav.no>
2024-04-10 11:49:43 +0200

    1253: DB opprydding

--- src/main/resources/db/migration/V1.0__drift_og_vedlikehold.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE drift_og_vedlikehold (
-                                    drift_og_vedlikehold_id BIGINT NOT NULL,
-                                    vedlikehold_modus BOOLEAN NOT NULL,
-                                    vedlikehold_overskrift TEXT NOT NULL,
-                                    vedlikehold_melding TEXT NOT NULL,
-                                    driftsmelding TEXT NOT NULL
-);
-
-INSERT INTO drift_og_vedlikehold (drift_og_vedlikehold_id, vedlikehold_modus, vedlikehold_overskrift, vedlikehold_melding, driftsmelding) VALUES (0, false, '', '', '');
--- src/main/resources/db/migration/V1.1__bruker.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE bruker (
-    ident TEXT NOT NULL,
-    rolle TEXT NOT NULL,
-    navn TEXT,
-    tilganger TEXT,
-    CONSTRAINT PK_bruker PRIMARY KEY (ident)
-);
