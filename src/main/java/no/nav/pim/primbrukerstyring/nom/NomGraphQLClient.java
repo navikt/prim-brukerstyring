7995e4595b90bb019dfcbcce6630135e44d5c121
Balder Riiser Haugerud <Balder.Riiser.Haugerud@nav.no>
2024-04-12 16:12:42 +0200

    1253: Flyttet sektor til ledere

--- src/main/java/no/nav/pim/primbrukerstyring/nom/NomGraphQLClient.java
+++ src/main/java/no/nav/pim/primbrukerstyring/nom/NomGraphQLClient.java
@@ -52,12 +52,12 @@ public class NomGraphQLClient {
                                         ressurs {
                                             navident
                                             visningsnavn
-                                            sektor
                                             ledere {
                                                 erDagligOppfolging
                                                 ressurs {
                                                     navident
                                                     visningsnavn
+                                                    sektor
                                                 }
                                             }
                                         }
@@ -70,12 +70,12 @@ public class NomGraphQLClient {
                                                 ressurs {
                                                     navident
                                                     visningsnavn
-                                                    sektor
                                                     ledere {
                                                         erDagligOppfolging
                                                         ressurs {
                                                             navident
                                                             visningsnavn
+                                                            sektor
                                                         }
                                                     }
                                                 }
