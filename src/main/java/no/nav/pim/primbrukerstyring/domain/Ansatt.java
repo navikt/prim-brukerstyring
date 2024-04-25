73cca9d054c9015cfe8389c36f2d0cbf1ccb7433
Balder Riiser Haugerud <Balder.Riiser.Haugerud@nav.no>
2024-04-25 12:53:02 +0200

    1253: Lagt inn ansatt til db struktur igjen

--- src/main/java/no/nav/pim/primbrukerstyring/domain/Ansatt.java
+++ src/main/java/no/nav/pim/primbrukerstyring/domain/Ansatt.java
@@ -1,5 +1,9 @@
 package no.nav.pim.primbrukerstyring.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import no.nav.pim.primbrukerstyring.nom.domain.NomRessurs;
 
@@ -8,12 +12,15 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Entity
+@Table(name = "ansatt")
 @ToString
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Ansatt {
 
     static public Ansatt fraNomRessurs(NomRessurs ressurs) {
@@ -29,21 +36,33 @@ public class Ansatt {
         return nyAnsatt;
     }
 
+    @Id
+    @SequenceGenerator(name = "sequence-generator", sequenceName = "brukerstyring_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence-generator")
+    @Column(name = "ansatt_id")
+    private Long ansattId;
+
+    @Column
+    @NotNull
     String ident;
 
+    @Column
+    @NotNull
     String navn;
 
+    @OneToMany(mappedBy = "ansatt")
+    @JsonBackReference
     Set<AnsattStillingsavtale> stillingsavtaler = new HashSet<>();
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Ansatt ansatt)) return false;
-        return Objects.equals(ident, ansatt.ident) && Objects.equals(navn, ansatt.navn) && Objects.equals(stillingsavtaler, ansatt.stillingsavtaler);
+        return Objects.equals(ansattId, ansatt.ansattId) && Objects.equals(ident, ansatt.ident) && Objects.equals(navn, ansatt.navn) && Objects.equals(stillingsavtaler, ansatt.stillingsavtaler);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(ident, navn, stillingsavtaler);
+        return Objects.hash(ansattId, ident, navn, stillingsavtaler);
     }
 }
