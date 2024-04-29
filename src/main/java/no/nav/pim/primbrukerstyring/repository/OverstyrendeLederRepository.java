edbe585dd04ab27acefdb8f3b1d5ed4b93f1c67b
Balder Riiser Haugerud <Balder.Riiser.Haugerud@nav.no>
2024-04-29 11:29:47 +0200

    1253: Skrivefeil i sql spørring

--- src/main/java/no/nav/pim/primbrukerstyring/repository/OverstyrendeLederRepository.java
+++ src/main/java/no/nav/pim/primbrukerstyring/repository/OverstyrendeLederRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 @Repository
 public interface OverstyrendeLederRepository extends JpaRepository<OverstyrendeLeder, String> {
 
-    @Query(value = "select * from overstyrende_leder where ident = ?1 and til = null", nativeQuery = true)
+    @Query(value = "select * from overstyrende_leder where ansatt_ident = ?1 and til = null", nativeQuery = true)
     Optional<OverstyrendeLeder> findByAnsattIdent(String ansattIdent);
 
     @Query(value = "select * from overstyrende_leder ol inner join leder l on l.leder_id = ol.overstyrende_leder_id where l.ident = ?1 and ol.til = null", nativeQuery = true)
