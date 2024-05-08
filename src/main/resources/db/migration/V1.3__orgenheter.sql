cb2d717b363782df151f6e36f31f42bf64f64b2b
Balder Riiser Haugerud <Balder.Riiser.Haugerud@nav.no>
2024-05-08 10:12:06 +0200

    1253: Rename til leder_leder_id

--- src/main/resources/db/migration/V1.3__orgenheter.sql
+++ src/main/resources/db/migration/V1.3__orgenheter.sql
@@ -1,6 +1,6 @@
 CREATE TABLE orgenhet (
    nom_id VARCHAR(255) NOT NULL,
    navn VARCHAR(255) NOT NULL,
-   leder_id BIGINT NOT NULL,
-   CONSTRAINT FK_orgenhet_leder FOREIGN KEY (leder_id) REFERENCES leder (leder_id)
+   leder_leder_id BIGINT NOT NULL,
+   CONSTRAINT FK_orgenhet_leder FOREIGN KEY (leder_leder_id) REFERENCES leder (leder_id)
 );
