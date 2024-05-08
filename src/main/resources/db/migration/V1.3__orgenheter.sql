9f4509830bf427cd40af58300289850d74633b46
Balder Riiser Haugerud <Balder.Riiser.Haugerud@nav.no>
2024-05-08 09:44:05 +0200

    1253: Enda en sql fiks

--- src/main/resources/db/migration/V1.3__orgenheter.sql
+++ src/main/resources/db/migration/V1.3__orgenheter.sql
@@ -2,5 +2,5 @@ CREATE TABLE orgenhet (
    nom_id VARCHAR(255) NOT NULL,
    navn VARCHAR(255) NOT NULL,
    leder_id BIGINT NOT NULL,
-   CONSTRAINT FK_orgenhet_leder FOREIGN KEY (leder_id) REFERENCES leder (id);
+   CONSTRAINT FK_orgenhet_leder FOREIGN KEY (leder_id) REFERENCES leder (id)
 );
